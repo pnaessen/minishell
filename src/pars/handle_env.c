f359bb01f10f6e4bde3b99fa17ba6ee74cc23911
VICTOIRE PERRIER <vicperri@mac.home>
2025-03-26 11:40:53 +0100

    trying to merge with the work of riko

--- src/pars/handle_env.c
+++ src/pars/handle_env.c
@@ -40,28 +40,79 @@ char	*handle_invalid_variable(char *args, int i)
 
 char	*find_and_replace_var(char *args, t_env **env)
 {
+	char **tab_args;
 	char *new_args;
 	int i;
+	int j;
 	t_data data;
 
-	new_args = ft_strdup(args);
+	tab_args = pre_tokenisation(args);
 	i = 0;
 	data.quote_type = '\0';
 	data.quote_num = 0;
 	data.quotes = ERROR;
-	while (args[i])
+	while (tab_args[i])
 	{
-		handle_quotes(args[i], &data);
-		if (args[i] == '$')
+		j = 0;
+		while (tab_args[i][j])
 		{
-			if (is_valid_var_char(args, i) == SUCCESS)
+			handle_quotes(tab_args[i][j], &data);
+			if (tab_args[i][j] == '$')
 			{
-				if (data.quote_type != 39)
-					new_args = handle_variable_replacement(args, i, &data, env);
+				if (is_valid_var_char(tab_args[i], j) == SUCCESS)
+				{
+					if (data.quote_type != 39)
+						tab_args[i] = handle_variable_replacement(tab_args[i], j, &data,
+								env);
+				}
+				else
+					tab_args[i] = handle_invalid_variable(tab_args[i], j);
 			}
-			else
-				new_args = handle_invalid_variable(args, i);
+			j++;
 		}
+		if (i > 0)
+
+		i++;
+	}
+	new_args = join_tabs(tab_args);
+	return (new_args);
+}
+int len_tab_of_tab(char **tab_args)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (tab_args[i])
+	{
+		j = 0;
+		while (tab_args[i][j])
+			j++;
+		i++;
+
+	}
+	return (i + j);
+}
+char *join_tabs(char **tab_args)
+{
+	int i;
+	int j;
+	int k;
+	int size;
+	char *new_args;
+
+	i = 0;
+	k = 0;
+	size = len_tab_of_tab(tab_args);
+	new_args = malloc((size) * sizeof(char));
+	if (!new_args)
+		return (NULL);
+	while (tab_args[i])
+	{
+		j = 0;
+		while (tab_args[i][j])
+			new_args[k++] = tab_args[i][j++];
+		new_args[k++] = ' ';
 		i++;
 	}
 	return (new_args);
